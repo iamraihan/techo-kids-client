387060ddf08bb1e7c75448ea7fbf60122927c691
iamraihan <trueraihan@gmail.com>
2022-08-11 21:42:44 +0600

    register form added

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -3,9 +3,26 @@ import { FaFacebook, FaGoogle } from "react-icons/fa";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import registerImg from "../images/teacher-student.webp";
 import auth from "../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors, isDirty, isSubmitting, touchedFields, submitCount },
+  } = useForm({
+    defaultValues: {
+      name: "",
+    },
+  });
+  let signError;
+  signError = (
+    <p className="text-error">
+      <small>{error?.message}</small>
+    </p>
+  );
   return (
     <div className="max-w-screen-xl mx-auto">
       <div className="grid grid-cols-2">
@@ -29,7 +46,8 @@ const Register = () => {
             </button>
           </div>
 
-          <form>
+          <form onSubmit={handleSubmit(console.log)}>
+            {/* name */}
             <div className="form-control w-full max-w-xs">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -38,11 +56,97 @@ const Register = () => {
                 type="text"
                 placeholder="Type Your Name"
                 className="input input-bordered w-full max-w-xs"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "Name is required",
+                  },
+                })}
               />
               <label className="label">
-                <span className="label-text-alt">Alt label</span>
+                {errors.name?.type === "required" && (
+                  <span className="label-text-alt text-error">
+                    {errors.name.message}
+                  </span>
+                )}
               </label>
             </div>
+            {/* email */}
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Type Your Email"
+                className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: "Please input a valid Email", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-error">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+              <label className="label">
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-error">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+              {/* password */}
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Type Your Password"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must input 6 character or more", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-error">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+              <label className="label">
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-error">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            {signError}
+            <input
+              className="w-full max-w-xs btn btn-primary hover:text-white hover:bg-secondary"
+              type="submit"
+              value="SignUp"
+            />
           </form>
         </div>
 
